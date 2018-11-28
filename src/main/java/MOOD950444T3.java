a623416b6438abbab7ceedacf92918182b407482
MustafaSnaid2018 <39595514+MustafaSnaid2018@users.noreply.github.com>
2018-11-28 11:42:27 +0200

    Create MOOD950444T3.java

---
+++ src/main/java/MOOD950444T3.java
@@ -0,0 +1,19 @@
+/**
+ * Hello world!
+ */
+public class MOOD950444T3
+{
+
+    private final String message = "Hello World!";
+
+    public MOOD950444T3() {}
+
+    public static void main(String[] args) {
+        System.out.println(new MOOD950444T3().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
